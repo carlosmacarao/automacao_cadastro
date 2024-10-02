a204ae11cda87352ec1abf0d946eb1691c5bb4c2
carlosmacarao <carlosmacarao9@gmail.com>
2024-10-03 00:47:02 +0200

    new change

--- automacao.py
+++ automacao.py
@@ -5,3 +5,8 @@ import time
 url_formulario = "https://forms.office.com/r/PwS0k9yyqs?origin=lprLink"
 
 subprocess.run(r"C:\Program Files\Google\Chrome\Application\chrome.exe")
+
+time.sleep(1)
+pyautogui.write(url_formulario)
+
+pyautogui.press('enter')

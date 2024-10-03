dfb8efd14f79072fbcf2a7b751a007059ece592d
carlosmacarao <carlosmacarao9@gmail.com>
2024-10-03 09:15:41 +0200

    new change on the main file

--- automacao.py
+++ automacao.py
@@ -9,4 +9,21 @@ subprocess.run(r"C:\Program Files\Google\Chrome\Application\chrome.exe")
 time.sleep(1)
 pyautogui.write(url_formulario)
 
+time.sleep(1)
 pyautogui.press('enter')
+
+#time.sleep(1)
+#pyautogui.press('tab')
+#pyautogui.press('tab')
+
+#time.sleep(0.5)
+pyautogui.write("Carlos")
+pyautogui.press('tab')
+
+time.sleep(0.5)
+pyautogui.write("Mozlista")
+pyautogui.press('tab')
+
+time.sleep(0.5)
+pyautogui.write("Programador")
+pyautogui.press('tab')
